39fec82c5edeb6baea958bcd4b36199efc86f7a3
Heron4gf <herongiambuzzi@gmail.com>
2026-02-19 16:49:24 +0100

    added folders

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { Sidebar } from '../../components/sidebar/sidebar';
 import { UploadContainer } from '../../components/upload-container/upload-container';
+import { NgIf } from '@angular/common';
+import { FolderPreview } from '../../components/folder-preview/folder-preview';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Sidebar, UploadContainer, NgOptimizedImage],
+  imports: [Sidebar, UploadContainer, NgOptimizedImage, NgIf,FolderPreview],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
