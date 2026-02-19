acd31322db9e6c0bbf0529d2a044de6162c2d6fd
Heron4gf <herongiambuzzi@gmail.com>
2026-02-19 17:12:20 +0100

    replaced deprecated methods

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,18 +2,23 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { Sidebar } from '../../components/sidebar/sidebar';
 import { UploadContainer } from '../../components/upload-container/upload-container';
-import { NgIf } from '@angular/common';
 import { FolderPreview } from '../../components/folder-preview/folder-preview';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Sidebar, UploadContainer, NgOptimizedImage, NgIf, FolderPreview],
+  imports: [Sidebar, UploadContainer, NgOptimizedImage, FolderPreview],
   templateUrl: './home.html',
   styleUrl: './home.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Home {
+
+  folders = Array.from({ length: 100 }, (_, i) => ({
+    label: `Cartella ${i + 1}`,
+    previewText: `Anteprima contenuto della cartella numero ${i + 1}...`
+  }));
+
   handleFile(file: File) {
     console.log('File received:', file);
   }
